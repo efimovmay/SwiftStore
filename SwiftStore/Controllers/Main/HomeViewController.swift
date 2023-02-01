6022f57db293afc36b1c64e3a20db98faf587343
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-02-01 21:52:14 +0300

    Add storyboards references, change Cart and Cataloge storyboards to table view with navigation

---
+++ SwiftStore/Controllers/Main/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  SwiftStore
+//
+//  Created by Виталий Гринчик on 1.02.23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
