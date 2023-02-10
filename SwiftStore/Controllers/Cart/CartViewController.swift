7cdb9e2190467a5cec4f374ede951eb9ee48e782
Aleksey Efimov <smaylik121@gmail.com>
2023-02-10 20:57:14 +0300

    connect label and button in cartVC

--- SwiftStore/Controllers/Cart/CartViewController.swift
+++ SwiftStore/Controllers/Cart/CartViewController.swift
@@ -10,35 +10,49 @@ import UIKit
 
 class CartViewController: UIViewController {
     
+    // MARK: - IBOutlet
 
     @IBOutlet var cartTableView: UITableView!
     @IBOutlet var buttonView: UIView!
+    
     @IBOutlet var emptyCartStack: UIStackView!
     
-    var nowInCart = [Product]()
+    @IBOutlet var numberItemInCartLabel: UILabel!
+    @IBOutlet var fullPriceLabel: UILabel!
+    
+    var cart: [Product]!
+    
+    // MARK: - Override Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
- //       nowInCart = Product.getRandomProducts(count: 13)
-        
         cartTableView.delegate = self
         cartTableView.dataSource = self
-        
-    }
-    override func viewWillLayoutSubviews() {
-        hideTable()
+
+        cart = Product.getRandomProducts(count: 13)
     }
     
+    override func viewWillLayoutSubviews() {
+        hideTable()
+        settingsUI()
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func designPressButton() {
+        let number = 333
+        showAlertController(title: "Заказ успешно оформлен",
+                            massege: "Номер вашего заказа: \(number)")
+    }
 }
-
 
 //MARK: - UI settings
 
 extension CartViewController {
     
-    func hideTable() {
-        if nowInCart.isEmpty {
+    private func hideTable() {
+        if cart == nil {
             cartTableView.isHidden = true
             buttonView.isHidden = true
             emptyCartStack.isHidden = false
@@ -48,6 +62,22 @@ extension CartViewController {
             emptyCartStack.isHidden = true
         }
     }
+    
+    private func showAlertController(title: String, massege: String) {
+        let alert = UIAlertController(title: title, message: massege, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okButton)
+        present(alert, animated: true)
+    }
+    
+    private func settingsUI() {
+        if cart != nil {
+            
+            let fullPrice = 333
+            numberItemInCartLabel.text = "\(cart.count) товаров"
+            fullPriceLabel.text = "\(fullPrice) $"
+        }
+    }
 }
 
 
@@ -55,17 +85,21 @@ extension CartViewController {
 
 extension CartViewController: UITableViewDelegate, UITableViewDataSource {
     
-     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return nowInCart.count
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if cart == nil {
+            return 0
+        } else {
+            return cart.count
+        }
     }
     
      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ItemCartTableViewCell
-        let product = nowInCart[indexPath.row]
+        let product = cart[indexPath.row]
         
         cell.productImage.image = UIImage(named: product.image)
         cell.nameLable.text = product.model
-        cell.priceLable.text = String(product.price) + " RUB"
+        cell.priceLable.text = "\(product.price) $"
         
         return cell
     }
