4d9dba3e244f87b3325277cce7ca5f1a1a2c67a7
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-02-11 23:51:48 +0300

    Change appearance of collection cells iat Home tab

--- SwiftStore/Controllers/Main/CollectionViewCell.swift
+++ SwiftStore/Controllers/Main/CollectionViewCell.swift
@@ -25,8 +25,6 @@ final class CollectionViewCell: UICollectionViewCell {
     }
     
     private func setupCellItem() {
-        // Setup cell view
-        backgroundColor = #colorLiteral(red: 0.9976320863, green: 0.9901652932, blue: 0.8451907039, alpha: 1)
         layer.cornerRadius = 15
         productImage.layer.cornerRadius = 15
         productImage.backgroundColor = .white
--- SwiftStore/Controllers/Main/CustomButton.swift
+++ SwiftStore/Controllers/Main/CustomButton.swift
@@ -9,28 +9,34 @@ import UIKit
 
 @IBDesignable final class CustomButton: UIButton {
 
-    var isTapped = false
     
-    // Button view constants
-    private let untappedColor = #colorLiteral(red: 0.9640280604, green: 0.8113391995, blue: 0.2740806341, alpha: 1)
+    // Button appearance constants
+    private let untappedColor = UIColor.white
     private let tappedColor = UIColor.lightGray
     
+    var isTapped = false
+    
     override func draw(_ rect: CGRect) {
-        // Initial button view
+        // Initial button appearance
         setButtonView(title: "Купить", isTapped: isTapped)
     }
     
+    // Configure title and appearance in accordance with button state
     func setButtonView(title: String, isTapped: Bool) {
-        setTitleColor(.black, for: .normal)
         layer.cornerRadius = frame.height / 2
-        
-        if isTapped {
-            setTitle(title, for: .normal)
-            layer.backgroundColor = tappedColor.cgColor
-            return
-        }
+        layer.borderWidth = 1
         setTitle(title, for: .normal)
-        layer.backgroundColor = untappedColor.cgColor
+    
+        if isTapped {
+            setTitleColor(.black, for: .normal)
+            layer.backgroundColor = tappedColor.cgColor
+            layer.borderColor = tappedColor.cgColor
+        } else {
+            setTitle(title, for: .normal)
+            setTitleColor(.systemBlue, for: .normal)
+            layer.backgroundColor = untappedColor.cgColor
+            layer.borderColor = UIColor.systemBlue.cgColor
+        }
         return
     }
 }
