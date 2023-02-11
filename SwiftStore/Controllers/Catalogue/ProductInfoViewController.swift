072c469bfb0db40e30c6cab1e5013440158d52be
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-02-11 16:39:26 +0500

    Products titles + Product specs
    On the product card, the list of product features now displays only those features that are in this product

--- SwiftStore/Controllers/Catalogue/ProductInfoViewController.swift
+++ SwiftStore/Controllers/Catalogue/ProductInfoViewController.swift
@@ -16,36 +16,89 @@ final class ProductInfoViewController: UIViewController, UITableViewDelegate, UI
     @IBOutlet weak var techSpecsTableView: UITableView!
     
     // MARK: - Public Properties
-//    var products = Product.getRandomProducts(count: 1)
     var product: Product?
     var cart: [Product]?
     
     // MARK: - Private Properties
-    private var productSpecs: [String] {
-        [
-            "Категория: \(product?.category ?? "")",
-            "Модель: \(product?.model ?? "")",
-            "Процессор: \(product?.chip ?? "")",
-            "Цвет: \(product?.color ?? "")",
-            "Диагональ: \(product?.display ?? "")",
-            "Оперативная память: \(product?.memory ?? "")",
-            "Память: \(product?.storage ?? "")"
-        ]
+    var productSpecs: [String] {
+        getSpecs(for: product!)
     }
-
+    
     // MARK: - Override ProductsViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTechSpecsTableView()
-        productImage.image = UIImage(named: product?.image ?? "")
-        productLabel.text = product?.image
+        setupView()
+        
     }
     
-    // MARK: - Table view data source
+    // MARK: - Private Methods
+    private func setupView() {
+        guard let product = product else { return }
+        setupTechSpecsTableView()
+        
+        productImage.image = UIImage(named: product.image )
+        productLabel.text = getTitle(for: product)
+    }
+    
+    private func setupTechSpecsTableView() {
+        let rowHeight = 60
+        techSpecsTableView.rowHeight = CGFloat(rowHeight)
+        techSpecsHeight.constant = CGFloat(rowHeight * productSpecs.count)
+    }
+    
+    private func getTitle(for product: Product) -> String {
+        switch product.category {
+        case "Mac":
+            if product.model.contains("M1") || product.model.contains("M2") {
+                return "\(product.model) \(product.color) \(product.memory)/\(product.storage)"
+            } else {
+                return "\(product.model) \(product.chip) \(product.color) \(product.memory)/\(product.storage)"
+            }
+        case "iPhone", "iPad":
+            return "\(product.model) \(product.color) \(product.storage)"
+        case "Watch":
+            return "\(product.model) \(product.display)"
+        default:
+            return "\(product.model)"
+        }
+    }
+    
+    private func getSpecs(for product: Product) -> [String] {
+        
+        var productSpecs: [String] = []
+        
+        if product.category != "" {
+            productSpecs.append("Категория: \(product.category)")
+        }
+        if product.model != "" {
+            productSpecs.append("Модель: \(product.model)")
+        }
+        if product.chip != "" {
+            productSpecs.append("Процессор: \(product.chip)")
+        }
+        if product.color != "" {
+            productSpecs.append("Цвет: \(product.color)")
+        }
+        if product.display != "" {
+            productSpecs.append("Диагональ: \(product.display)")
+        }
+        if product.memory != "" {
+            productSpecs.append("Оперативная память: \(product.memory)")
+        }
+        if product.storage != "" {
+            productSpecs.append("Память: \(product.storage)")
+        }
+        
+        return productSpecs
+    }
+}
+
+// MARK: - Table view data source
+extension ProductInfoViewController {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         productSpecs.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "techSpec", for: indexPath)
@@ -59,12 +112,4 @@ final class ProductInfoViewController: UIViewController, UITableViewDelegate, UI
         
         return cell
     }
-    
-    // MARK: - Private Methods
-    private func setupTechSpecsTableView() {
-        let rowHeight = 60
-        techSpecsTableView.rowHeight = CGFloat(rowHeight)
-        techSpecsHeight.constant = CGFloat(rowHeight * productSpecs.count)
-    }
-    
 }
