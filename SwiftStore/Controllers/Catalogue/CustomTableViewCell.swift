e3f2c9da9d57fd1591fd6e7e4123a15a19736f55
Kirilloao <kirilltr@list.ru>
2023-02-13 18:10:24 +0100

    Change bar item image. Add custom button

--- SwiftStore/Controllers/Catalogue/CustomTableViewCell.swift
+++ SwiftStore/Controllers/Catalogue/CustomTableViewCell.swift
@@ -12,19 +12,16 @@ class CustomTableViewCell: UITableViewCell {
     @IBOutlet weak var productImageView: UIImageView!
     @IBOutlet weak var productNameLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
-    @IBOutlet weak var buyButton: UIButton!
+    @IBOutlet weak var buyButton: CustomButton!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        buyButton.layer.cornerRadius = buyButton.frame.height / 2
-        buyButton.backgroundColor = #colorLiteral(red: 0.9640280604, green: 0.8113391995, blue: 0.2740806341, alpha: 1)
-        buyButton.setTitleColor(.black, for: .normal)
-        buyButton.setTitle("Купить", for: .normal)
-    }
-    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//
+//    }
     @IBAction func buyButtonDidTapped() {
-//            buyButton.configuration = setupButton(with: "В корзине")
-        buyButton.setTitle("В корзине", for: .normal)
-        buyButton.backgroundColor = .lightGray
+        if !buyButton.isTapped {
+            buyButton.setButtonView(withTitle: "В корзине", for: .tapped)
+            buyButton.isTapped = true
+        }
     }
 }
