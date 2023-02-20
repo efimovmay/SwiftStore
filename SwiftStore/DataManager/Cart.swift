9bc13c05d6ebd63b5505e0c43b88214fdaf7d70b
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-02-20 21:12:32 +0300

    Modify Cart manager: add computable property isEmpty and method contains(_:) -> Bool

--- SwiftStore/DataManager/Cart.swift
+++ SwiftStore/DataManager/Cart.swift
@@ -9,10 +9,18 @@ class Cart {
     
     static var shared = Cart()
     
+    static var isEmpty: Bool {
+        Cart.shared.cart.isEmpty
+    }
+    
     var cart = [Product]()
     
     init() {}
     
+    static func contains(_ product: Product) -> Bool {
+        Cart.shared.cart.contains(product)
+    }
+
     // For testing
     #warning("Delete this method on closing the project")
     static func printOut(from viewController: String) {
@@ -20,5 +28,4 @@ class Cart {
         Cart.shared.cart.forEach { output.append($0.model + " | ") }
         print(viewController + ": " + (Cart.shared.cart.isEmpty ? "EMPTY" : output))
     }
-
 }
